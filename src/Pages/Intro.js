4e93dbbec91d3d021f63b46ca5aff47c5fc15962
thutra1999 <trathutran9@gmail.com>
2022-07-05 12:05:11 +0700

    Intro.js

---
+++ src/Pages/Intro.js
@@ -0,0 +1,58 @@
+
+const Intro = () => {
+    return (
+      <>
+      <div class="container-fluid ">    
+  <div class="row content">
+    <div class="col-sm-2 sidenav">
+      {/* <!-- <p><a href="#">Link</a></p>
+      <p><a href="#">Link</a></p>
+      <p><a href="#">Link</a></p> --> */}
+    </div>
+    <div class="col-sm-8 text-left"> 
+      <div class="text-center">
+      <h4>GIỚI THIỆU</h4>
+      <p>Cửa hàng Pet Shop</p>
+      </div>
+     
+ 
+      <p>Puppy Pet Shop được mở bởi các thành viên chung niềm đam mê nuôi dưỡng, chăm sóc thú cưng. Bán hàng với giá rẻ nhất thị trường không phải là mục tiêu của Puppy Pet Shop, chất lượng sản phẩm và dịch vụ mới là tiêu chí hàng đầu.
+
+        Puppy Pet Shop hướng tới lợi ích tối ưu cho khách hàng qua việc đem tới những sản phẩm có chất lượng tốt nhất với mức giá hợp lý nhất. Với phương châm tạo sự tiện lợi và thoải mái nhất cho khách hàng, từ việc tư vấn chi tiết chủng loại, giá cả cho khách hàng đến việc giao hàng và chăm sóc khách hàng, Puppy Pet Shop luôn quan tâm để ý một cách chu đáo nhất.</p>
+    
+        <p>
+            <span><b>Trụ sở chính:</b></span>
+            <span> 36 Hoàng Cầu, Đống Đa, Hà Nội</span>
+        </p>
+        <p>
+            <span><b>Cơ sở 2:</b></span>
+            <span> 24 Hoàng Quốc Việt, Nghĩa Đô, Cầu Giấy, Hà Nội</span>
+        </p>
+        <div >
+						<span >
+            <span><b>Hotline:</b></span>
+						</span>
+						<a href="tel:0869125610"> 0869125610</a>
+					</div>
+        <div >
+						<span >
+            <span><b>Email:</b></span>
+						</span>
+						<a href="mailto:nguyenquoctrungk69@gmail.com"> nguyenquoctrungk69@gmail.com</a>
+					</div>
+    </div>
+    <div class="col-sm-2 sidenav">
+      {/* <!-- <div class="well">
+        <p></p>
+      </div>
+      <div class="well">
+        <p></p>
+      </div> --> */}
+    </div>
+  </div>
+</div>
+      </>
+    )
+  };
+  
+  export default Intro;
