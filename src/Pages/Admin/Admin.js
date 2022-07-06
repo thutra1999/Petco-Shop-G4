b76f2d0a46b1b60dd60c757c319abad240269d4d
lagiahuy <103893858+lagiahuy@users.noreply.github.com>
2022-07-06 14:32:39 +0700

    Update Admin.js

--- src/Pages/Admin/Admin.js
+++ src/Pages/Admin/Admin.js
@@ -1,63 +1,117 @@
-import React from 'react';
-import { useState } from 'react';
-import { useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import ProductTable from './ProductTable';
+import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
+import { Link } from "react-router-dom";
+import ProductTable from "./ProductTable";
 
 function Admin() {
-  const [searchTerm, setSearchTerm] = useState('');
+  const [searchTerm, setSearchTerm] = useState("");
   const [products, setProducts] = useState([]);
+  const [filter, setFilter] = useState("");
+  const [page, setPage] = useState(true);
+
+  const categoryFilterHandler = (props) => {
+    setFilter(props);
+  };
 
   useEffect(() => {
-    console.log('app useeffect!!');
-    let url = 'https://62b421ada36f3a973d2c998f.mockapi.io/testShop';
+    let url = "https://62b421ada36f3a973d2c998f.mockapi.io/testShop";
     if (searchTerm.length > 0) {
-      url = url + '?search=' + searchTerm;
+      url = url + "?search=" + searchTerm;
     }
-
-    console.log(url);
+    if (filter.length > 0) {
+      url = url + "?category=" + filter;
+    }
     fetch(url)
       .then((response) => response.json())
       .then((data) => {
         setProducts(data);
       });
-  }, [searchTerm]);
+  }, [searchTerm, filter]);
+
+  useEffect(() => {
+    let url = "https://62b04ad2e460b79df0424941.mockapi.io/id";
+    if (searchTerm.length > 0) {
+      url = url + "?phone=" + searchTerm;
+    }
+
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        setProducts(data);
+      });
+  }, [searchTerm, filter]);
 
   return (
-    <div className="container">
-      <h2>Danh sách sản phẩm</h2>
-      <div class="row">
-        <div class="col-sm-6">
-          <div className="input-group">
-            <input
-              type="text"
-              className="form-control"
-              value={searchTerm}
-              onChange={(e) => setSearchTerm(e.target.value)}
-            ></input>
-            <div className="input-group-append">
-              <button
-                className="btn btn-secondary"
-                type="button"
-              >
-                <i class="fa fa-search"></i>
-              </button>
+    <>
+      <button
+        onClick={() => {
+          setPage(true);
+          setSearchTerm("");
+        }}
+      >
+        Quản lý sản phẩm
+      </button>
+      <button
+        onClick={() => {
+          setPage(false);
+          setSearchTerm("");
+        }}
+      >
+        Quản lý đơn hàng
+      </button>
+      {page ? (
+        <div className="container">
+          <h2>Danh sách sản phẩm</h2>
+          <div class="row">
+            <div class="col-sm-6">
+              <div className="input-group">
+                <input
+                  type="text"
+                  className="form-control"
+                  value={searchTerm}
+                  onChange={(e) => setSearchTerm(e.target.value)}
+                ></input>
+                <div className="input-group-append">
+                  <button className="btn btn-secondary" type="button">
+                    <i class="fa fa-search"></i>
+                  </button>
+                </div>
+              </div>
+              <p>
+                <Link to={"/productedit/new"}>
+                  <button className="btn btn-primary" type="button">
+                    Thêm mới sản phẩm
+                  </button>
+                </Link>
+              </p>
             </div>
           </div>
-          <p>
-            <Link to={'/productedit/new'}>
-              <button
-                className="btn btn-primary"
-                type="button"
-              >
-                Thêm mới sản phẩm
-              </button>
-            </Link>
-          </p>
+          <ProductTable data={products} filter={categoryFilterHandler} />
         </div>
-      </div>
-      <ProductTable data={products} />
-    </div>
+      ) : (
+        <div className="container">
+          <h2>Danh sách đơn hàng</h2>
+          <div class="row">
+            <div class="col-sm-6">
+              <div className="input-group">
+                <input
+                  type="text"
+                  className="form-control"
+                  value={searchTerm}
+                  onChange={(e) => setSearchTerm(e.target.value)}
+                ></input>
+                <div className="input-group-append">
+                  <button className="btn btn-secondary" type="button">
+                    <i class="fa fa-search"></i>
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
 
