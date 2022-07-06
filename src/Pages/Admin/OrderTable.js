f0d86309c47bd09ecefcddafe96dad0b1a7e2bf0
lagiahuy <103893858+lagiahuy@users.noreply.github.com>
2022-07-06 17:32:50 +0700

    Update OrderTable.js

--- src/Pages/Admin/OrderTable.js
+++ src/Pages/Admin/OrderTable.js
@@ -33,32 +33,10 @@ function OrderTable(props) {
         <td>{item.phone}</td>
         <td>{item.email}</td>
         <td>{item.cart.length}</td>
-        {/* <td>
-          <strong>{item.price} VND</strong>
-        </td>
-        <td>{item.category}</td>
-        <td className={item.status ? "bg-success" : "bg-danger"}>
-          <div class="text-light text-center">
-            {item.status ? "Còn hàng" : "Hết"}
-          </div>
-        </td> */}
+        
         <td>
-          <Link to={"/orderdetail/" + item.id}>Chi tiết sản phẩm</Link>
+          <Link to={"/orderdetail/" + item.id}>Chi tiết đơn hàng</Link>
         </td>
-        {/* <td>
-          <Link to={"/productedit/" + item.id}>
-            <button type="button" className="btn btn-primary">
-              Sửa
-            </button>
-          </Link>
-          <button
-            type="button"
-            className="btn btn-warning"
-            onClick={() => deleteUser(item.id)}
-          >
-            <i className="fa fa-trash text-danger" aria-hidden="true"></i>
-          </button>
-        </td> */}
       </tr>
     ));
   }
@@ -69,51 +47,11 @@ function OrderTable(props) {
         <tr>
           <th onClick={() => sortColumn("id", "number")}>Mã đơn hàng</th>
           <th onClick={() => sortColumn("name", "string")}>Tên khách hàng</th>
-
-          {/* <th onClick={() => sortColumn("price", "number")}>
-            Đơn giá<span> </span>
-          </th> */}
           <th>Địa chỉ</th>
           <th>Điện thoại</th>
           <th>Email</th>
           <th>Số lượng sản phẩm</th>
-          <th>Chi tiết</th>
-          {/* <th class="nav-item dropdown">
-            <a
-              class="text-light dropdown-toggle"
-              href="#"
-              id="navbarDropdown"
-              role="button"
-              data-bs-toggle="dropdown"
-              aria-expanded="false"
-            >
-              Phân loại
-            </a>
-            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-              <li>
-                <a class="text-light " onClick={() => props.filter('')}>Tất cả</a>
-              </li>
-              <li>
-                <a class="text-light " onClick={() => props.filter('Thức ăn')}>Thức ăn</a>
-              </li>
-              <li>
-                <a class="text-light" onClick={() => props.filter('Thuốc')}>Thuốc</a>
-              </li>
-              <li>
-                <a class="text-light" onClick={() => props.filter('Đồ dùng')}>Đồ dùng</a>
-              </li>
-              <li>
-                <a class="text-light" onClick={() => props.filter('Quần áo')}>Quần áo</a>
-              </li>
-              <li>
-                <a class="text-light"onClick={() => props.filter('Khác')}>Khác</a>
-              </li>
-            </ul>
-          </th> */}
-          {/* <th>Trạng thái</th>
-          <th colSpan={2}>
-            <br />
-          </th> */}
+          <th><br/></th>
         </tr>
       </thead>
       <tbody>{order_list}</tbody>
