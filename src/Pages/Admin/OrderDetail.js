3c4058dc6bb2dafeba0f1c402db70c3c7156e562
lagiahuy <103893858+lagiahuy@users.noreply.github.com>
2022-07-06 17:32:47 +0700

    Update OrderDetail.js

--- src/Pages/Admin/OrderDetail.js
+++ src/Pages/Admin/OrderDetail.js
@@ -1,135 +1,117 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
-import { useState } from 'react';
-import { useEffect } from 'react';
-import './OrderDetail.css';
+import React from "react";
+import { useParams } from "react-router-dom";
+import { useState } from "react";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 const OrderDetail = () => {
-    const params = useParams();
-    const [orders, setOrders] = useState(null);
+  let navigate = useNavigate();
+  const params = useParams();
+  const [orders, setOrders] = useState([]);
 
-    useEffect(() => {
-        console.log('user use effect!!');
+  useEffect(() => {
+    let url = "https://62b04ad2e460b79df0424941.mockapi.io/id/" + params.id;
 
-        let url =
-            'https://62b04ad2e460b79df0424941.mockapi.io/id/' + params.id;
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        setOrders(data);
+      });
+  }, []);
 
-        console.log(url);
-        fetch(url)
-            .then((response) => response.json())
-            .then((data) => {
-                // data.dob = new Date(data.dob);
-                setOrders(data);
-            });
-    }, []);
+  console.log(orders);
+  console.log(orders.id);
+  console.log(orders.useName);
+  console.log(orders.cart);
 
-    var list_order = orders.cart;
-//     var category_jsx = [];
-//   if (orders != null) {
-//     category_jsx = (
-//       <ul className="nav navbar-nav">
-//         {
-//           (category_jsx = categories.map((item) => (
-//             <li>
-//               <Link to={"/shop/" + item.category_name}>
-//                 <a>{item.category_name}</a>
-//               </Link>
-//             </li>
-//           )))
-//         }
-//       </ul>
-//     );
-//   }
+  var cartList = [];
+  if (orders.cart != null) {
+    cartList = orders.cart.map((item) => (
+      <ul className="nav navbar-nav">
+        <li class="row">
+          <div class="col-6">{item.name}</div>
+          <div class="col-1">x{item.quantity}</div>
+        </li>
+      </ul>
+    ));
+  }
 
-    return (
-        <>
-            {orders != null ? (
-                <table class="table">
-                    <thead>
-                        <tr>
-                            <th scope="col">Id</th>
-                            <th scope="col">Tên sản phẩm</th>
-                            <th scope="col">Số lượng</th>
-                            <th scope="col">Giá sản phẩm</th>
-                            <th scope="col">Giá sản phẩm</th>
-                            <th scope="col">Ảnh mô tả</th>
-                        </tr>
-                    </thead>
+  return (
+    <>
+      {orders != null ? (
+        <div class="container">
+          <div class="panel-body inf-content">
+            <div class="row">
+              <div class="col">
+                <strong class="h3">Thông tin đơn hàng</strong>
+                <br />
+                <div class="table-responsive">
+                  <table class="table table-user-information">
                     <tbody>
-                        {/* <tr>
-                            <td class="text-primary">{orders.cart[id]}</td>
-                            <td class="text-primary">{orders.cart.name}</td>
-                            <td class="text-primary">{orders.cart.quantity}</td>
-                            <td class="text-primary">{orders.cart.price}</td>
-                            <td class="text-primary">{orders.cart.discount}</td>
-                            <td class="text-primary"><img src={orders.cart.picture}></img></td>
-                        </tr> */}
+                      <tr>
+                        <td>
+                          <strong>Mã đơn hàng</strong>
+                        </td>
+                        <td class="text-primary">{orders.id}</td>
+                      </tr>
+                      <tr>
+                        <td>
+                          <strong>Tên khách hàng</strong>
+                        </td>
+                        <td class="text-primary">{orders.useName}</td>
+                      </tr>
+                      <tr>
+                        <td>
+                          <strong>Số điện thoại</strong>
+                        </td>
+                        <td class="text-primary">{orders.phone}</td>
+                      </tr>
+                      <tr>
+                        <td>
+                          <strong>Địa chỉ</strong>
+                        </td>
+                        <td class="text-primary">{orders.address}</td>
+                      </tr>
+
+                      <tr>
+                        <td>
+                          <strong>Email</strong>
+                        </td>
+                        <td class="text-primary">{orders.email}</td>
+                      </tr>
+                      <tr>
+                        <td>
+                          <strong>Thành phần đơn hàng</strong>
+                        </td>
+                        <td>{cartList}</td>
+                      </tr>
+                      <tr>
+                      <td>
+                          <strong>Tổng giá trị đơn hàng</strong>
+                        </td>
+                        <td class="text-primary">
+                            {orders.price}
+                        </td>
+                      </tr>
                     </tbody>
-                </table>
-                // <div class="container">
-                //   <div class="panel-body inf-content">
-                //     <div class="row">
-                //       <div class="col">
-                //         <strong class="h3">Thông tin sản phẩm </strong>
-                //         <br />
-                //         <div class="table-responsive">
-                //           <table class="table table-user-information">
-                //             <tbody>
-                //               <tr>
-                //                 <td>
-                //                   <strong>Mã sản phẩm</strong>
-                //                 </td>
-                //                 <td class="text-primary">{orders.cart.id}</td>
-                //               </tr>
-                //               <tr>
-                //                 <td>
-                //                   <strong>Tên sản phẩm</strong>
-                //                 </td>
-                //                 <td class="text-primary">{orders.cart.name}</td>
-                //               </tr>
-                //               <tr>
-                //                 <td>
-                //                   <strong>Số lượng</strong>
-                //                 </td>
-                //                 <td class="text-primary">{orders.cart.quantity}</td>
-                //               </tr>
-                //               <tr>
-                //                 <td>
-                //                   <strong>Giá sản phẩm</strong>
-                //                 </td>
-                //                 <td class="text-primary">{orders.cart.price}</td>
-                //               </tr>
-                //               <tr>
-                //                 <td>
-                //                   <strong>Giảm giá</strong>
-                //                 </td>
-                //                 <td class="text-primary">{orders.cart.discount}</td>
-                //               </tr>
-                //               <tr>
-                //                 <td>
-                //                   <strong>Ảnh sản phẩm</strong>
-                //                 </td>
-                //                 <td class="text-primary"><img src={orders.cart.picture}></img></td>
-                //               </tr>
-                //             </tbody>
-                //           </table>
-                //         </div>
-                //       </div>
-                //     </div>
-                //   </div>
-                //   <Link to={'/productedit/' + params.id}>
-                //     <button type="button" className="btn btn-primary">
-                //       Chỉnh sửa
-                //     </button>
-                //   </Link>
-                //   <Link to="/admin">
-                //   <button type="button" class="btn btn-secondary">Trở lại</button></Link>
-                // </div>
-            ) : (
-                <div className='loader'></div>
-            )}
-        </>
-    );
+                  </table>
+                </div>
+              </div>
+            </div>
+          </div>
+          <button
+            type="button"
+            class="btn btn-secondary"
+            onClick={() => navigate(-1)}
+          >
+            Trở lại
+          </button>
+        </div>
+      ) : (
+        "loading"
+      )}
+    </>
+  );
 };
 
 export default OrderDetail;
