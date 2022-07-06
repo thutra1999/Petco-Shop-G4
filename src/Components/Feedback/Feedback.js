7bb3aec64b71d1b658883c4da6f50e210d4a4f12
thutra1999 <trathutran9@gmail.com>
2022-07-06 14:42:18 +0700

    Create Feedback.js

---
+++ src/Components/Feedback/Feedback.js
@@ -0,0 +1,65 @@
+import React, { useRef, useState } from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/scrollbar";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "./Feedback.css";
+import fb01 from "../../img/feedback01.jpg"
+import fb02 from "../../img/feedback02.jpg"
+import fb03 from "../../img/feedback03.jpg"
+import fb04 from "../../img/feedback04.jpg"
+import fb05 from "../../img/feedback05.jpg"
+
+// import required modules
+import { Keyboard, Scrollbar, Navigation, Pagination } from "swiper";
+
+function Feedback() {
+  return (
+    <>
+      <Swiper
+        slidesPerView={1}
+        centeredSlides={false}
+        slidesPerGroupSkip={1}
+        grabCursor={true}
+        keyboard={{
+          enabled: true,
+        }}
+        breakpoints={{
+          769: {
+            slidesPerView: 2,
+            slidesPerGroup: 2,
+          },
+        }}
+        scrollbar={true}
+        navigation={true}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Keyboard, Scrollbar, Navigation, Pagination]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <img src={fb01} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={fb02} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={fb03} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={fb04} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={fb05} />
+        </SwiperSlide>
+      </Swiper>
+    </>
+  );
+}
+
+export default Feedback
