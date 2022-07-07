2d7f486c22178f25604eb758156345877036ed9e
lagiahuy <103893858+lagiahuy@users.noreply.github.com>
2022-07-07 17:20:03 +0700

    Update ProductItem.js

--- src/Components/ProductItem/ProductItem.js
+++ src/Components/ProductItem/ProductItem.js
@@ -89,24 +89,24 @@ function ProductItem(props) {
 
       <div className="row">{list_product}</div>
       <ReactPaginate
-        previousLabel="<<"
-        nextLabel=">>"
-        pageclassName="page-item"
-        pageLinkclassName="page-link"
-        previousclassName="page-item"
-        previousLinkclassName="page-link"
-        nextclassName="page-item"
-        nextLinkclassName="page-link"
-        breakLabel="..."
-        breakclassName="page-item"
-        breakLinkclassName="page-link"
-        pageCount={pageCount}
-        marginPagesDisplayed={2}
-        pageRangeDisplayed={5}
-        onPageChange={handlePageClick}
-        containerclassName="pagination"
-        activeclassName="active"
-        forcePage={page}
+         previousLabel="Previous"
+         nextLabel="Next"
+         pageClassName="page-item"
+         pageLinkClassName="page-link"
+         previousClassName="page-item"
+         previousLinkClassName="page-link"
+         nextClassName="page-item"
+         nextLinkClassName="page-link"
+         breakLabel="..."
+         breakClassName="page-item"
+         breakLinkClassName="page-link"
+         pageCount={pageCount}
+         marginPagesDisplayed={2}
+         pageRangeDisplayed={5}
+         onPageChange={handlePageClick}
+         containerClassName="pagination"
+         activeClassName="active"
+         forcePage={page}
       />
 
     </>
