899785d3576e58721a7ef2f6dde7f1f487aed2ed
lagiahuy <103893858+lagiahuy@users.noreply.github.com>
2022-07-07 10:07:37 +0700

    Create AdminLayout.js

---
+++ src/Pages/AdminLayout.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+const AdminLayout = () => {
+  return (
+    <>
+      <Outlet />
+    </>
+  );
+};
+
+export default AdminLayout;
