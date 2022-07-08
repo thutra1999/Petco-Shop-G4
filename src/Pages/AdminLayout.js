896272b3988c7007bc7f4d4bf09df2fbc6a04fc5
PVQuyet <93429125+PVQuyet@users.noreply.github.com>
2022-07-08 09:47:18 +0700

    Update AdminLayout.js

--- src/Pages/AdminLayout.js
+++ src/Pages/AdminLayout.js
@@ -1,8 +1,22 @@
 import React from 'react';
-import { Outlet } from 'react-router-dom';
+import { Outlet, Link } from 'react-router-dom';
 const AdminLayout = () => {
   return (
     <>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+          <Link class="navbar-brand" to="/admin">Admin</Link>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+              <Link class="nav-link" aria-current="page" to="/admin/productmanager">Quản lý sản phẩm</Link>
+              <Link class="nav-link" to="/admin/ordermanager">Quản lý đơn hàng</Link>
+            </div>
+          </div>
+        </div>
+      </nav>
       <Outlet />
     </>
   );
