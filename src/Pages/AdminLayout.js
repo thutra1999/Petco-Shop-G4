0eb06089691362291efa53d8792467c0f64caea4
lagiahuy <103893858+lagiahuy@users.noreply.github.com>
2022-07-08 11:02:04 +0700

    Update AdminLayout.js

--- src/Pages/AdminLayout.js
+++ src/Pages/AdminLayout.js
@@ -3,16 +3,16 @@ import { Outlet, Link } from 'react-router-dom';
 const AdminLayout = () => {
   return (
     <>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container-fluid">
-          <Link class="navbar-brand" to="/admin">Admin</Link>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid">
+          <Link className="navbar-brand" to="/admin">Admin</Link>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
           </button>
-          <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-            <div class="navbar-nav">
-              <Link class="nav-link" aria-current="page" to="/admin">Quản lý sản phẩm</Link>
-              <Link class="nav-link" to="/admin/ordermanager">Quản lý đơn hàng</Link>
+          <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div className="navbar-nav">
+              <Link className="nav-link" aria-current="page" to="/admin">Quản lý sản phẩm</Link>
+              <Link className="nav-link" to="/admin/ordermanager">Quản lý đơn hàng</Link>
             </div>
           </div>
         </div>
