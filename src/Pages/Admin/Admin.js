1e1d7425e294230be1a19e83e9fb51fc2f92b891
lagiahuy <103893858+lagiahuy@users.noreply.github.com>
2022-07-08 10:47:20 +0700

    Update Admin.js

--- src/Pages/Admin/Admin.js
+++ src/Pages/Admin/Admin.js
@@ -1,113 +1,75 @@
 import React from "react";
+import ProductTable from "./ProductTable";
 import { useState } from "react";
 import { useEffect } from "react";
 import { Link } from "react-router-dom";
-import OrderTable from "./OrderTable";
-import ProductTable from "./ProductTable";
+import { Loader } from "../../Components/Loader/Loader";
+
 
 function Admin() {
-  const [products, setProducts] = useState([]);
-  const [searchTerm, setSearchTerm] = useState("");
-  const [page, setPage] = useState(true);
-  const [filter, setFilter] = useState("");
-  const [orders, setOrders] = useState([]);
+    const [products, setProducts] = useState([]);
+    const [searchTerm, setSearchTerm] = useState("");
+    const [filter, setFilter] = useState("");
+    const [loadItem, setLoadItem] = useState(true);
+    useEffect(() => {
+        
+   
+        let url = "https://62b421ada36f3a973d2c998f.mockapi.io/testShop";
+        if (searchTerm.length > 0) {
+            url = url + "?search=" + searchTerm;
+        }
+        if (filter.length > 0) {
+            url = url + "?category=" + filter;
+        }
+        setLoadItem(true);
+        fetch(url)
+            .then((response) => response.json())
+            .then((data) => {
+                setProducts(data)
+            }).then(() => setLoadItem(false));
+    }, [searchTerm, filter]);
 
 
+    const categoryFilterHandler = (props) => {
+        setFilter(props);
+    };
 
 
-
-  useEffect(() => {
-    let url = "https://62b421ada36f3a973d2c998f.mockapi.io/testShop";
-    if (searchTerm.length > 0) {
-      url = url + "?search=" + searchTerm;
-    }
-    if (filter.length > 0) {
-      url = url + "?category=" + filter;
-    }
-    fetch(url)
-      .then((response) => response.json())
-      .then((data) => {
-        setProducts(data)
-      });
-  }, [searchTerm, filter]);
-
-  useEffect(() => {
-    let url_oder = "https://62b04ad2e460b79df0424941.mockapi.io/id/";
-    if (searchTerm.length > 0) {
-      url_oder = url_oder + "?phone=" + searchTerm;
-    }
-
-    fetch(url_oder)
-      .then((response) => response.json())
-      .then((data) => {
-        setOrders(data);
-      });
-  }, [searchTerm]);
-
-
-  const categoryFilterHandler = (props) => {
-    setFilter(props);
-  };
-
-  return (
-    <>
-      {page ? (
-
-        <div className="container">
-          <h2>Danh sách sản phẩm</h2>
-          <div className="row">
-            <div className="col-sm-6">
-              <div className="input-group">
-                <input
-                  type="text"
-                  className="form-control"
-                  value={searchTerm}
-                  onChange={(e) => setSearchTerm(e.target.value)}
-                ></input>
-                <div className="input-group-append">
-                  <button className="btn btn-secondary" type="button">
-                    <i className="fa fa-search"></i>
-                  </button>
-                </div>
-              </div>
+    return (
+        <>
+            <div className="container">
+                <h2>Danh sách sản phẩm</h2>
+                <div className="row">
+                    <div className="col-sm-6">
+                        <div className="input-group">
+                            <input
+                                type="text"
+                                className="form-control"
+                                value={searchTerm}
+                                onChange={(e) => setSearchTerm(e.target.value)}
+                            ></input>
+                            <div className="input-group-append">
+                                <button className="btn btn-secondary" type="button">
+                                    <i className="fa fa-search"></i>
+                                </button>
+                            </div>
+                        </div><br />
+                    </div>
+                    <div className="col-sm-6">
+                        <p>
+                            <Link to={"/admin/productedit/new"}>
+                                <button className="btn btn-primary" type="button">
+                                    Thêm mới sản phẩm
+                                </button>
+                            </Link>
+                        </p>
+                    </div>
+                </div><br /><br />
+                {!loadItem ?
+                <ProductTable data={products} filter={categoryFilterHandler} />: <Loader/>}
             </div>
-            <div className="col-sm-6">
-              <p>
-                <Link to={"/admin/productedit/new"}>
-                  <button className="btn btn-primary" type="button">
-                    Thêm mới sản phẩm
-                  </button>
-                </Link>
-              </p>
-            </div>
-          </div>
-          <ProductTable data={products} filter={categoryFilterHandler} />
-        </div>
-      ) : (
-        <div className="container">
-          <h2>Danh sách đơn hàng</h2>
-          <div className="row">
-            <div className="col-sm-6">
-              <div className="input-group">
-                <input
-                  type="text"
-                  className="form-control"
-                  value={searchTerm}
-                  onChange={(e) => setSearchTerm(e.target.value)}
-                ></input>
-                <div className="input-group-append">
-                  <button className="btn btn-secondary" type="button">
-                    <i className="fa fa-search"></i>
-                  </button>
-                </div>
-              </div>
-            </div>
-            <OrderTable data={orders} />
-          </div>
-        </div>
-      )}
-    </>
-  );
+        </>
+    )
 }
 
 export default Admin;
