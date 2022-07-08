4016d35330f07b3b5f7fb1a0d1d989c5f34cb660
lagiahuy <103893858+lagiahuy@users.noreply.github.com>
2022-07-08 14:24:21 +0700

    Create index.js

---
+++ src/usecontact/index.js
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from "react";
+
+const AuthContext = React.createContext({
+  isLoggedIn: false
+});
+
+export const AuthContextProvider = (props) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const storeUserLoggedInInformation = localStorage.getItem("isLoggedIn");
+    if (storeUserLoggedInInformation) {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
+  const logoutHandler = () => {
+    localStorage.removeItem("isLoggedIn");
+    setIsLoggedIn(false);
+  };
+  const loginHandler = (props) => {
+    localStorage.setItem("isLoggedIn", props);
+    setIsLoggedIn(true);
+  };
+  return (
+    <AuthContext.Provider
+      value={{
+        isLoggedIn: isLoggedIn,
+        onLogout: logoutHandler,
+        onLogin: loginHandler,
+      }}
+    >
+      {props.children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthContext;
